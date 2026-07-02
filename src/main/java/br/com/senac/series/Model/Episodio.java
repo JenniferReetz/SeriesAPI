5194e34cb475be2862fd682dbbb5ed1926568cfa
ianestacio07-arch <ianestacio07@gmail.com>
2026-07-02 19:31:07 -0300

    Feito model

--- src/main/java/br/com/senac/series/Model/Episodio.java
+++ src/main/java/br/com/senac/series/Model/Episodio.java
@@ -1,4 +1,94 @@
 package br.com.senac.series.Model;
 
 public class Episodio {
+
+    private int id;
+
+    private String titulo;
+
+    private int duracao;
+
+    private int temporada;
+
+    private int idSerie;
+
+    public Episodio() {
+
+    }
+
+    public Episodio(int id, String titulo, int duracao, int temporada, int idSerie) {
+
+        this.id = id;
+
+        this.titulo = titulo;
+
+        this.duracao = duracao;
+
+        this.temporada = temporada;
+
+        this.idSerie = idSerie;
+
+    }
+
+    public int getId() {
+
+        return id;
+
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+    public String getTitulo() {
+
+        return titulo;
+
+    }
+
+    public void setTitulo(String titulo) {
+
+        this.titulo = titulo;
+
+    }
+
+    public int getDuracao() {
+
+        return duracao;
+
+    }
+
+    public void setDuracao(int duracao) {
+
+        this.duracao = duracao;
+
+    }
+
+    public int getTemporada() {
+
+        return temporada;
+
+    }
+
+    public void setTemporada(int temporada) {
+
+        this.temporada = temporada;
+
+    }
+
+    public int getIdSerie() {
+
+        return idSerie;
+
+    }
+
+    public void setIdSerie(int idSerie) {
+
+        this.idSerie = idSerie;
+
+    }
+
 }
+ 
--- src/main/java/br/com/senac/series/Model/Serie.java
+++ src/main/java/br/com/senac/series/Model/Serie.java
@@ -1,4 +1,41 @@
 package br.com.senac.series.Model;
 
 public class Serie {
+
+    private int id;
+    private String nome;
+    private String genero;
+
+    public Serie() {
+    }
+
+    public Serie(int id, String nome, String genero) {
+        this.id = id;
+        this.nome = nome;
+        this.genero = genero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
